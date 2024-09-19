89ffd0fa2523dc43dd863cb087eb095442aced1e
Didi Alfandi <dialfandi25@gmail.com>
2024-09-19 15:48:12 +0700

    Update Deffuzification Function using CoS

--- Fuzzy-2.c
+++ Fuzzy-2.c
@@ -26,6 +26,10 @@ struct member_output
     float hig_result;
 } duration;
 
+struct shape_of_cos
+{
+    float A[3];
+} shape_of_cos, xa;
 // fungsi menghitung nilai max
 float max(float a, float b) { return a > b ? a : b; }
 
@@ -146,13 +150,62 @@ void fuzzifikasi(bool member, float input)
     }
 }
 
+float flexible_upper_line(float myu, float a, float b, float c, float d)
+{
+    return (d - a) - (((d - a) - (c - b)) * myu);
+}
+
+float set_bottom_line(bool mode, float a, float b)
+{
+    if (mode)
+    {
+        return b - a;
+    }
+    else
+    {
+        return (b + a) / 2;
+    }
+}
+
+float one_constrain_shape(unsigned int shape, float myu, float a, float b, float c, float d)
+{
+    if (shape == 0) // trapezidal_full
+    {
+        return (((d - a) + (c - b)) / 2) * myu;
+    }
+    else if (shape == 1) // triangle_full
+    {
+        return ((c - a) * myu) / 2;
+    }
+}
+
+float deffuzi_CoS(float myu1, float myu2, float myu3)
+{
+    if (myu1 == 1 || myu2 == 1 || myu3 == 1)
+    {
+        shape_of_cos.A[0] = one_constrain_shape(0, myu1, 0, 0, 8, 12);
+        shape_of_cos.A[1] = one_constrain_shape(1, myu2, 10, 0, 20, 0);
+        shape_of_cos.A[2] = one_constrain_shape(0, myu3, 12, 22, 30, 30);
+    }
+    else if (myu1 < 1 || myu2 < 1 || myu3 < 1)
+    {
+        shape_of_cos.A[0] = ((flexible_upper_line(myu1, 0, 0, 8, 12) + set_bottom_line(1, 0, 12)) * myu1) / 2;
+        shape_of_cos.A[1] = ((flexible_upper_line(myu2, 10, 0, 0, 20) + set_bottom_line(1, 10, 20)) * myu2) / 2;
+        shape_of_cos.A[2] = ((flexible_upper_line(myu3, 18, 22, 30, 30) + set_bottom_line(1, 18, 30)) * myu3) / 2;
+    }
+    xa.A[0] = set_bottom_line(0, 12, 0);
+    xa.A[1] = set_bottom_line(0, 10, 20);
+    xa.A[2] = set_bottom_line(0, 18, 30);
+    return (((shape_of_cos.A[0] * xa.A[0]) + (shape_of_cos.A[1] * xa.A[1]) + (shape_of_cos.A[2] * xa.A[2])) / (shape_of_cos.A[0] + shape_of_cos.A[1] + shape_of_cos.A[2]));
+}
+
 void inference(int max_count)
 {
-    //Set to 0 value
+    // Set to 0 value
     duration.low_result = 0;
     duration.med_result = 0;
     duration.hig_result = 0;
-    //Created Rule Base "Minimum Selection"
+    // Created Rule Base "Minimum Selection"
     duration.low[0] = min(temperature.member[0], humidity.member[1]);
     duration.low[1] = min(temperature.member[2], humidity.member[1]);
     duration.low[2] = min(temperature.member[0], humidity.member[2]);
@@ -168,20 +221,21 @@ void inference(int max_count)
     printf("============> DEGREE OF MEMBERSHIP FUZZY MINIMA <=============\n");
     for (int i = 0; i < max_count; i++)
     {
-        //printf("Durasi Penyiraman (duration) 'Lama': %.2f\n", duration.hig_result);
+        // printf("Durasi Penyiraman (duration) 'Lama': %.2f\n", duration.hig_result);
         printf("==========>LOOP KE-%d<==========\n", i);
         printf("duration LOW: %.2f\n", duration.low[i]);
         printf("duration MEDIUM: %.2f\n", duration.medium[i]);
         printf("duration HIGH: %.2f\n", duration.high[i]);
-        //seleksi nilai terbesar
+        // seleksi nilai terbesar
         if (i >= 0 && i < count_low)
         {
             if (duration.low[i] > duration.low_result)
             {
                 duration.low_result = duration.low[i];
             }
-            else{
-                //printf("CHECK3\n");
+            else
+            {
+                // printf("CHECK3\n");
             }
         }
         if (i >= 0 && i < count_med)
@@ -190,8 +244,9 @@ void inference(int max_count)
             {
                 duration.med_result = duration.medium[i];
             }
-            else{
-                //printf("CHECK2\n");
+            else
+            {
+                // printf("CHECK2\n");
             }
         }
         if (i >= 0 && i < count_hig)
@@ -199,10 +254,11 @@ void inference(int max_count)
             if (duration.high[i] > duration.hig_result)
             {
                 duration.hig_result = duration.high[i];
-                //printf("Durasi Penyiraman (duration) 'Lama': %.2f\n", duration.hig_result);
+                // printf("Durasi Penyiraman (duration) 'Lama': %.2f\n", duration.hig_result);
             }
-            else{
-                //printf("CHECK3\n");
+            else
+            {
+                // printf("CHECK3\n");
             }
         }
     }
@@ -212,7 +268,7 @@ void inference(int max_count)
     printf("Durasi Penyiraman (duration) 'Lama': %.2f\n", duration.hig_result);
 }
 
-float deffuzi_CoG(float low_minmax, float med_minmax, float high_minmax, float low_set, float medium_set, float high_set)
+float deffuzi_WG(float low_minmax, float med_minmax, float high_minmax, float low_set, float medium_set, float high_set)
 {
     return ((low_minmax * low_set) + (med_minmax * medium_set) + (high_minmax * high_set)) / (low_minmax + med_minmax + high_minmax);
 }
@@ -241,9 +297,14 @@ int main()
         printf("Lembab  : %.2f\n", humidity.member[2]);
 
         inference(max_array_count);
-        float result_of_CoG = deffuzi_CoG(duration.low_result, duration.med_result, duration.hig_result, 10, 20, 30);
-        printf("============> RESULT OF DEFFUZIFICATION <=============\n");
-        printf("Result: %.2f menit\n", result_of_CoG);
+        float result_of_WG = deffuzi_WG(duration.low_result, duration.med_result, duration.hig_result, 8, 15, 22);
+        float result_of_CoS = deffuzi_CoS(duration.low_result, duration.med_result, duration.hig_result);
+        printf("============> RESULT OF DEFFUZIFICATION 1<=============\n");
+        //printf("shape1: %.2f || %.2f || %.2f || %.2f\n", shape_of_cos.A[0], xa.A[0], flexible_upper_line(duration.low_result, 0, 0, 8, 12), set_bottom_line(1, 0, 12) * duration.low_result / 2);
+        //printf("shape2: %.2f || %.2f\n", shape_of_cos.A[1], xa.A[1]);
+        //printf("shape3: %.2f || %.2f\n", shape_of_cos.A[2], xa.A[2]);
+        printf("Result WeG: %.2f menit\n", result_of_WG);
+        printf("Result CoS: %.2f menit\n", result_of_CoS);
 
         printf("Continue The Fuzzy System? ");
         scanf("%d", &input_count);
